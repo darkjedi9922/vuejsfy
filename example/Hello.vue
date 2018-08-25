54307bcd89366e6d42d0f2c1516e98d8394ecc61
Jed Dark <dartsidiys777@mail.ru>
2018-08-25 11:41:13 +0300

    Hello VueJSfy

---
+++ example/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+    <p class="hello">Hello, {{ who }}</p>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            who: 'VueJSfy'
+        }
+    }
+}
+</script>
+
+<style>
+.hello {
+    font-weight: bold;
+}
+</style>
